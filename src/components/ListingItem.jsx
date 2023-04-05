b34410235424d25c9b35c2ee6717337ea79d6852
osidel <osidel79@gmail.com>
2023-04-05 14:52:37 -0700

    add delete and edit functionality to my listing section

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Moment  from "react-moment"
 import {MdLocationOn} from "react-icons/md"
+import {FaTrash} from "react-icons/fa"
+import {MdEdit} from "react-icons/md"
 
-export default function ListingItem({listing, id}) {
+export default function ListingItem({listing, id, onEdit, onDelete}) {
     
   return (
     <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-lg rounded-md overflow-hidden transition-shadow duration-150 m-[10px] '>
@@ -32,8 +34,17 @@ export default function ListingItem({listing, id}) {
                         <p className='font-bold text-xs'>{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
                     </div>
                 </div>
-            </div>
+            </div>           
         </Link>
+        { onDelete && (
+            <FaTrash 
+            className='absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500' 
+            onClick={() => onDelete(listing.id)} />
+        )}
+        { onEdit && (
+             <MdEdit className='absolute bottom-2 right-10 h-4 cursor-pointer text-black-500' 
+             onClick={() => onEdit(listing.id)} />
+         )}
     </li>
   )
 }
